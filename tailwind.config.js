d995abb364c237696a99007462fbba946332a233
Bryan Assunção <110056473+bryan-assuncao@users.noreply.github.com>
2024-10-21 18:09:47 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  purge: false, // Desabilita a purgação de classes
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
